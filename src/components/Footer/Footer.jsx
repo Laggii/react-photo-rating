b5e6b7b2651855d187dc90108e35682e472e00c4
Laggii <ivan.alekseev.w@gmail.com>
2017-09-08 16:36:38 +0300

    fixed url for repo

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export const Footer = () => (
   <div className='core-footer'>
-    <a href='https://github.com/Laggii'>@Laggii, 2017</a>
+    <a href='https://github.com/Laggii/react-photo-rating'>@Laggii, 2017</a>
   </div>
 );
 
